781a6a3d131f97e4763f3929da71adc50831d458
stariel <ariel.altaras@gmail.com>
2018-01-13 13:07:48 -0800

    styling with CSS

--- scripts/views/book-list-page.js
+++ scripts/views/book-list-page.js
@@ -8,11 +8,11 @@ var app = app || {};
   bookListPage.initIndexView = (books) => {
     $('#book-list').empty();
     books.forEach(book => {
-      $('#book-list').append(`<li data-id="${book.book_id}">${book.title} by ${book.author}</li> <li><img src="${book.image_url}">`)
+      $('#book-list').append(`<div><p data-id="${book.book_id}">${book.title} by ${book.author}</p><img src="${book.image_url}"></div>`)
     })
     $('#book-count').text('Total Books: ' + books.length);
 
-    $('#book-list').on('click', 'li', (event) => {
+    $('#book-list').on('click', 'p', (event) => {
       const id = $(event.target).data('id')
       page('/books/' + id)
 
